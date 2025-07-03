25407f780e49b2adc950ddd6da0be7ddf39becd4
ozora-ogino <ozora.ogino.1127@gmail.com>
2025-07-04 00:01:41 +0900

    Add text embedding (temp implementation

--- dags/main.py
+++ dags/main.py
@@ -5,6 +5,7 @@ from airflow.operators.python import PythonOperator
 
 from crawler import TempCrawler
 from preprocessor import main as preprocessor_main
+from text_embedding import main as text_embeddgin_main
 
 
 dag = DAG(
@@ -17,10 +18,12 @@ crawl_op = PythonOperator(task_id="crawl_njss_csv",
                          python_callable=TempCrawler.crawl_njss_csv,
                          dag=dag)
 
-
-
 preprocess_op = PythonOperator(task_id="preprocess",
                          python_callable=preprocessor_main,
                          dag=dag)
 
-crawl_op >> preprocess_op
+text_embed_op = PythonOperator(task_id="text_embedding",
+                         python_callable=text_embeddgin_main,
+                         dag=dag)
+
+crawl_op >> preprocess_op >> text_embed_op
