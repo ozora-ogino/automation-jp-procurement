d23c0a4c456e74ff75b1188a1b834b0e9eec528a
Ozora Ogino <ozora.ogino.1127@gmail.com>
2025-07-08 14:57:10 +0900

    Add crawler

--- dags/main.py
+++ dags/main.py
@@ -3,7 +3,7 @@ import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
-from crawler import TempCrawler
+from crawler import main as crawler_main
 from preprocessor import main as preprocessor_main
 from text_embedding import main as text_embeddgin_main
 from llm import main as llm_main
@@ -12,12 +12,17 @@ from llm import main as llm_main
 dag = DAG(
     dag_id="main",
     schedule_interval=None,
+    start_date=datetime.datetime(2024, 1, 1),
+    catchup=False,
 )
 
-TempCrawler = TempCrawler()
-crawl_op = PythonOperator(task_id="crawl_njss_csv",
-                         python_callable=TempCrawler.crawl_njss_csv,
-                         dag=dag)
+# Use the main function from crawler that downloads to /data/search_result.csv
+crawl_op = PythonOperator(
+    task_id="crawl_njss_csv",
+    python_callable=crawler_main,
+    provide_context=True,
+    dag=dag
+)
 
 preprocess_op = PythonOperator(task_id="preprocess",
                          python_callable=preprocessor_main,
